1af41da5f5e82fc49250562818b8b71bb1592476
Anurag Sen <anuragsen@Anurags-MacBook-Air-3.local>
2023-12-02 15:53:06 +0530

    configured Docker file and wait for db file

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,7 +1,9 @@
 # Test file for the app
-from django.test import SimpleTestCase 
+from django.test import SimpleTestCase
 from app import calc
+
+
 
 class TestCalc(SimpleTestCase):
     def test_add(self):
-        self.assertEqual(calc.add(3,8), 11)
+        self.assertEqual(calc.add(3, 8), 11)
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,7 +7,9 @@ from sqlite3 import OperationalError as sqlite3OpError
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
-from django.conf import settings
+from django.conf import settings # noqa
+
+
 
 class Command(BaseCommand):
     """Django command to wait for database."""
@@ -25,9 +27,3 @@ class Command(BaseCommand):
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
-
-
-
-
-
-
