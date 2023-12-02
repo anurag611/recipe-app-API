fe3201bb1fa9ff72a59391ba9bd80bbb0f398a9f
Anurag Sen <anuragsen@Anurags-MacBook-Air-3.local>
2023-12-02 14:02:38 +0530

    configured Docker file and wait for db file

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,19 +1,27 @@
-from _sqlite3 import OperationalError as Sqlite3OperationalError
-from django.core.management.base import BaseCommand
+"""
+Django command to wait for the database to be available.
+"""
 import time
+
+from _sqlite3 import OperationalError as sqlite3OpError
+
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
+    """Django command to wait for database."""
 
     def handle(self, *args, **options):
+        """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
         while db_up is False:
             try:
-                self.check(databases='default')
+                self.check(databases=['default'])
                 db_up = True
-            except (Sqlite3OperationalError, OperationalError):
+            except (sqlite3OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
