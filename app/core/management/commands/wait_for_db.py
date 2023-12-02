a75b992f6a5d07d85d9226e6f45664da8d494851
Anurag Sen <anuragsen@Anurags-MacBook-Air-3.local>
2023-12-02 13:55:28 +0530

    configured Docker file and wait for db file

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                self.check(databases=['default'])
+                self.check(databases='default')
                 db_up = True
             except (Sqlite3OperationalError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
