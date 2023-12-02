59c51e05b640fcb5af9de35a0fdf01d228125dd3
Anurag Sen <anuragsen@Anurags-MacBook-Air-3.local>
2023-12-02 14:14:06 +0530

    configured Docker file and wait for db file

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,7 +3,7 @@ Django command to wait for the database to be available.
 """
 import time
 
-from _sqlite3 import OperationalError as sqlite3OpError
+from sqlite3 import OperationalError as sqlite3OpError
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
