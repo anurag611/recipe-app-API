8c5b84a49b779700903ebcbd2f7a9092c64315eb
Anurag Sen <anuragsen@Anurags-MacBook-Air-3.local>
2023-12-02 13:50:17 +0530

    configured Docker file and wait for db file

---
+++ app/app/tests.py
@@ -0,0 +1,7 @@
+# Test file for the app
+from django.test import SimpleTestCase 
+from app import calc
+
+class TestCalc(SimpleTestCase):
+    def test_add(self):
+        self.assertEqual(calc.add(3,8), 11)
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+
+from _sqlite3 import OperationalError as Sqlite3OperationalError
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTest(SimpleTestCase):
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for db when db is available"""
+    
+        patched_check.return_value = True
+        call_command('wait_for_db')
+        patched_check.assert_called_once_with(databases='default')
+
+    @patch('time.sleep')
+    def test_wait_for_db(self, patched_sleep, patched_check):
+            patched_check.side_effect = [Sqlite3OperationalError] * 2 +[OperationalError] * 3 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(patched_check.call_count, 6)
+            patched_check.assert_called_with(databases='default')
