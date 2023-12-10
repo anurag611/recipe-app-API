d2efca2ae7b62b59720eef83abe4b5fbf8ac7703
Anurag Sen <anuragsen@Anurags-MacBook-Air-3.local>
2023-12-10 14:22:36 +0530

    configured user model API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -60,7 +60,7 @@ class PublicUserApiTests(TestCase):
             'name': 'Test Name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
-   
+
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
