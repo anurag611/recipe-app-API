91a7bd3b4ad1364835d7b4dc2e2231a406868b69
Anurag Sen <anuragsen@Anurags-MacBook-Air-3.local>
2023-12-13 16:48:53 +0530

    configure tags and ingredients APIs

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -79,7 +79,7 @@ class PrivateIngredientsApiTests(TestCase):
         self.client.patch(url, payload)
         ingredient.refresh_from_db()
         self.assertEqual(ingredient.name, payload['name'])
- 
+
     def test_delete_ingredient(self):
         """Test deleting an ingredient."""
         ingredient = Ingredient.objects.create(user=self.user, name='Vinegar')
